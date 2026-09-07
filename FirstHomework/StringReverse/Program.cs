cf6fbb21a6ce47f13b826c4255f202f2fef0585c
ibckfv <islambbbbbbbbbbbbbb@gmail.com>
2026-09-07 19:34:18 +0300

    add homework

---
+++ FirstHomework/StringReverse/Program.cs
@@ -0,0 +1,22 @@
+﻿
+string? ReverseWord(string word)
+{
+    return new string(word.ToCharArray().Reverse().ToArray());
+}
+
+string? ReverseSentence(string sentence)
+{
+    var split = sentence.Split(" ")
+        .Select(i =>
+            i = new string(i.ToCharArray().Reverse().ToArray()));
+    
+    /*for (int i = 0; i < split.Length; i++)
+    {
+        split[i] = new string(split[i].ToCharArray().Reverse().ToArray());
+    }*/
+
+    return string.Join(" ", split);
+}
+
+Console.WriteLine(ReverseWord("Привет"));
+Console.WriteLine(ReverseSentence("Привет , мир"));
